97f448d1d4a871ab96a429a493773587bf4f2224
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-04-29 21:36:18 +0300

    Initial commit

---
+++ src/zadanie2_2_1.java
@@ -0,0 +1,11 @@
+// Задание здесь: https://lms.synergy.ru/learning/view/68552/?groupPeriodId=1045153&disciplineVersionId=25858&start=1
+// 1.	Выведите числа от 0 до миллиона
+public class zadanie2_2_1 {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x <= 30) {
+            System.out.print("\t" + x);
+            ++x;
+        }
+    }
+}
