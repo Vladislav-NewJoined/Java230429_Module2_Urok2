a49620cc4739b51d6a812ea7b002a75b3a91086f
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-04-29 22:38:40 +0300

    Initial commit

--- src/task2_2_1.java
+++ src/task2_2_1.java
@@ -2,10 +2,15 @@
 // 1.	Выведите числа от 0 до миллиона
 public class task2_2_1 {
     public static void main(String[] args) {
-        int x = 0;
-        while (x <= 10000) {
-            System.out.print(" " + x);
-            x++;
-        }
+//        int x1 = 0;
+//        while (x1 <= 100000) {
+//            System.out.print(" " + x);
+//            System.out.println(x1);
+//            x1++;
+//        }
+        System.out.println("Задание: \n1.\tВыведите числа от 0 до миллиона");
+        System.out.println();
+        System.out.println("Решение: \nК сожалению моя версия Intellij IDEA (IntelliJ IDEA 2022.3.2 (Community Edition)) не позволяет вывести в \nконсоль такое большое количество чисел. Обучающие ресурсы в интернет подсказывают, что путем \nизменения настроек в Intellij IDEA можно увеличить размер памяти консоли, но я не уверен, что у\nпреподавателя настройки IDE позволят вывести в консоль миллион чисел. По умолчанию код для решения\nэтой задачи вот такой: \npublic class task2_2_1 {\n     public static void main(String[] args) {\n       int x = 0;\n        while (x <= 1000000) {\n           System.out.println(x);\n            x++;\n        }\n    }\n}");
+
     }
 }
