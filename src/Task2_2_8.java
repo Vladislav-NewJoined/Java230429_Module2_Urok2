e51aa6eac8360e2b3b0fb92af01589d6d46589af
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-06 15:27:23 +0300

    Initial commit

--- src/Task2_2_8.java
+++ src/Task2_2_8.java
@@ -9,8 +9,6 @@ import java.util.Scanner;
 // Видеоурок здесь: https://lms.synergy.ru/learning/view/68494
 
 public class Task2_2_8 {
-
-    //        Пример 10 ППППППППППППППППППППППППППППП УДАЛСЯ!!!!!!!!!!!!!!!!!! ( Доработанный Пример 3 )
     public static void main(String[] args) {
         System.out.println("Задание: \n8.\tПервый игрок сохраняет слово и подсказку в первый файл, второй игрок во второй. \nИгра начинается, выводятся подсказки, игроки по-очереди пытаются отгадать слово \nпротивника.\n");
         System.out.println("Решение: ");
@@ -65,29 +63,29 @@ public class Task2_2_8 {
         while (true) {
             System.out.println("Первый игрок: введите строку (т.е. предполагаемое секретное слово): ");
             str_FromFirstPl = scanner_FromFirst_Player.nextLine();
-            if ((secrWord_Of_Second_Player.contains(str_FromFirstPl)) && (!str_FromFirstPl.equals(secrWord_Of_Second_Player)) && (!str_FromFirstPl.equals(""))) {
+                if ((secrWord_Of_Second_Player.contains(str_FromFirstPl)) && (!str_FromFirstPl.equals(secrWord_Of_Second_Player)) && (!str_FromFirstPl.equals(""))) {
                 System.out.println("Горячо.");
-            } else if
-            ((!secrWord_Of_Second_Player.contains(str_FromFirstPl)) || (!str_FromFirstPl.equals(secrWord_Of_Second_Player)) || (str_FromFirstPl.equals(""))) {
-                System.out.println("Не верно.");
-            } else {
-                System.out.println("Правильно!");
-                System.out.println("Победил первый игрок.");
-                break;
-            }
+                    } else if
+                        ((!secrWord_Of_Second_Player.contains(str_FromFirstPl)) || (!str_FromFirstPl.equals(secrWord_Of_Second_Player)) || (str_FromFirstPl.equals(""))) {
+                        System.out.println("Не верно.");
+                    } else {
+                    System.out.println("Правильно!");
+                    System.out.println("Победил первый игрок.");
+                    break;
+                    }
 
             System.out.println("Второй игрок: введите строку (т.е. предполагаемое секретное слово): ");
             str_FromSecondPl = scanner_FromSecond_Player.nextLine();
-            if ((secrWord_Of_First_Player.contains(str_FromSecondPl)) && (!str_FromSecondPl.equals(secrWord_Of_First_Player)) && (!str_FromSecondPl.equals(""))) {
+                if ((secrWord_Of_First_Player.contains(str_FromSecondPl)) && (!str_FromSecondPl.equals(secrWord_Of_First_Player)) && (!str_FromSecondPl.equals(""))) {
                 System.out.println("Горячо.");
-            } else if
-            ((!secrWord_Of_First_Player.contains(str_FromSecondPl)) || (!str_FromSecondPl.equals(secrWord_Of_First_Player)) || (str_FromSecondPl.equals(""))) {
-                System.out.println("Не верно.");
-            } else {
-                System.out.println("Правильно!");
-                System.out.println("Победил второй игрок.");
-                break;
-            }
+                    } else if
+                    ((!secrWord_Of_First_Player.contains(str_FromSecondPl)) || (!str_FromSecondPl.equals(secrWord_Of_First_Player)) || (str_FromSecondPl.equals(""))) {
+                    System.out.println("Не верно.");
+                    } else {
+                    System.out.println("Правильно!");
+                    System.out.println("Победил второй игрок.");
+                    break;
+                    }
         }
-    } //        Конец Примера 10 ККККККККККККККК  ( Доработанный Пример 3 )
+    }
 }
